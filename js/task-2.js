cabb90c785f4e2d2306539a5bbe27bb633c0dec7
Oleksandr197 <sealanch73@gmai.com>
2020-08-16 16:16:32 +0300

    Work-1

---
+++ js/task-2.js
@@ -0,0 +1,10 @@
+const ordered = 150;
+const total = 100;
+
+const message =
+ordered <= total
+    ? "Заказ оформлен, с вами свяжется менеджер"
+    : "На складе недостаточно товаров!";
+
+console.log(message);
+
